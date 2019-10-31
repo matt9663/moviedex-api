aa2cf6a14a4378c29e8569b8e4d395db60725702
Matt Wood <matt9663@gmail.com>
2019-10-31 15:31:00 -0400

    configs added for Heroku deployment

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require('cors')
 const MOVIES = require('./movies-data.json')
 
 const app = express()
-
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -38,7 +38,18 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies);
 
-const PORT = 8000
+app.use((error, req, req, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error'}}
+    }
+    else { 
+        response = {error}
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
